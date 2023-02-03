7a3c05562ac26336c522b0d58b69a9faa3964629
Brian Antunes <brian.antunes@yahoo.com>
2023-02-03 18:00:38 -0500

    Added to controller, models, and Dtos

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,17 +1,35 @@
 using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CharacteController : ControllerBase
+    public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
-
-        public IActionResult Get()
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService characterService)
         {
-            return Ok(knight);
+            _characterService = characterService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
     }
 }
